957926cb7362dd11fceb72b962035ab71f80a67d
Nibraaska <nnibraas@gmail.com>
2019-06-07 04:15:45 -0400

    Cleaned up

--- collagewebsite/collagewebsite/settings.py
+++ collagewebsite/collagewebsite/settings.py
@@ -122,7 +122,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-
-MEDIA_URL = 'media/'
-
-MEDIA_ROOT = os.path.join(BASE_DIR, MEDIA_URL)
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
+MEDIA_URL = '/media/'
